bb64e84247afd635f42695f5750f175b6e7fb658
Valentijn Hol <git@hirtol.top>
2024-04-16 16:51:39 +0200

    Expand benchmarks a bit

--- pg_graph/benches/divan.rs
+++ pg_graph/benches/divan.rs
@@ -1,4 +1,5 @@
 use std::path::PathBuf;
+use pg_graph::ParityGame;
 
 fn main() {
     divan::main();
@@ -12,18 +13,29 @@ mod solver_benches {
 
     #[divan::bench(args = super::GAMES)]
     fn bench_small_progress(bencher: divan::Bencher, game: &str) {
-        bencher.with_inputs(|| {
-            let path = super::example_dir().join(game);
-            let data = std::fs::read_to_string(&path).unwrap();
-            let graph = pg_parser::parse_pg(&mut data.as_str()).unwrap();
-            let parity_game = pg_graph::ParityGame::new(graph).unwrap();
-            parity_game
-        }).bench_values(|parity_game| {
+        bencher.with_inputs(|| super::load_pg(game)).bench_values(|parity_game| {
             let mut game = solvers::small_progress::SmallProgressSolver::new(&parity_game);
 
             game.run();
         });
     }
+
+    #[divan::bench(args = super::GAMES)]
+    fn bench_zielonka(bencher: divan::Bencher, game: &str) {
+        bencher.with_inputs(|| super::load_pg(game)).bench_values(|parity_game| {
+            let mut game = solvers::zielonka::ZielonkaSolver::new(&parity_game);
+
+            game.run();
+        });
+    }
+}
+
+pub fn load_pg(game: &str) -> ParityGame {
+    let path = crate::example_dir().join(game);
+    let data = std::fs::read_to_string(&path).unwrap();
+    let graph = pg_parser::parse_pg(&mut data.as_str()).unwrap();
+    let parity_game = pg_graph::ParityGame::new(graph).unwrap();
+    parity_game
 }
 
 pub fn example_dir() -> PathBuf {
